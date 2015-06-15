e6c0ab17d2d627671d71d89a64f9ae82964fbe09
HirofumiSakurai <skrihrfm@plum.plala.or.jp>
2015-06-16 06:49:45 +0900

    done ex.10

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -20,12 +20,28 @@ describe "MicropostPages" do
       end
     end
 
+    it { should have_content('0 microposts')}
+
     describe "with valid information" do
       before do
         fill_in 'micropost_content', with: "Lerem ipsum"
       end
       it "should create a micropost" do
         expect {click_button("Post")}.to change(Micropost, :count). by(1)
+      end
+
+      describe "should have correct micropost count" do
+        before { click_button "Post"}
+
+        it { should have_content('1 micropost')}
+
+        describe "with another post" do
+          before do
+            fill_in 'micropost_content', with: "test, again"
+            click_button "Post"
+          end
+          it { should have_content('2 microposts')}
+        end
       end
     end
   end
@@ -40,5 +56,47 @@ describe "MicropostPages" do
         expect {click_link "delete"}.to change(Micropost, :count).by(-1)
       end
     end
+  end
+end
+
+describe "with many microposts" do
+  subject {page}
+
+  before(:all) do
+    @user = FactoryGirl.create(:user)
+    @microposts = 50.times {FactoryGirl.create(:micropost, user: @user, content: "test")}
+    visit root_path
+  end
+  after(:all) do
+    @user.microposts.each do |micropost|
+      micropost.destroy
+    end
+    @user.destroy
+  end
+
+  describe "pagination" do
+    before {sign_in @user}
+    it {should have_selector('div.pagination')}
+  end
+
+  # it "should list each microposts" do
+  #   @user.microposts.paginate(page:1).each do |micropost|
+  #     expect(page).to have_selector('li', text: micropost.content)
+  #   end
+  # end
+
+  describe "exist delete link on current user" do
+    before do
+      sign_in @user
+    end
+    it {should have_content('delete')}
+  end
+
+  describe "no delete link on other user's login" do
+    let (:other_user) {FactoryGirl.create(:user)}
+    before do
+      sign_in other_user
+    end
+    it {should_not have_content('delete')}
   end
 end
